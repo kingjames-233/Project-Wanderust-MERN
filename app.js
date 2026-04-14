da1f5eb9d5686657ff9bc3ffc04109ed97791c30
kingjames-233 <kingjames2061@gmail.com>
2026-04-14 17:59:25 +0545

    Update app.js

--- app.js
+++ app.js
@@ -64,10 +64,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.render("home.ejs");
-// });
-
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
